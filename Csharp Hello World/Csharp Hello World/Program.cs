e79d2edd76d41bcae5d60783779dbc6a4a45776f
Landsberger_Mike <Landsberger_Mike@G-02-19>
2022-08-31 08:42:03 -0400

    First Commit

---
+++ Csharp Hello World/Csharp Hello World/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Howdy. What is your name?");
+            string input = Console.ReadLine();
+            Console.WriteLine($"It's nice to have you {input}");
+            Console.ReadKey();
+        }
+    }
+}
